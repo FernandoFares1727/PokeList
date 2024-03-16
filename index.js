2727cb793ee7e8c1b06c3d2ccbe8dd6a5b2f969e
FernandoFares1727 <fernandimfares@gmail.com>
2024-03-16 15:37:52 -0300

    Create poke cards

---
+++ index.js
@@ -0,0 +1,55 @@
+const pokeRange = 200;
+
+const mainUrl = "https://pokeapi.co/api/v2/";
+const limitParam = "pokemon?limit=";
+
+const componentsFolder = "./Components/";
+const pokeCardPage = "pokeCard.html";
+
+//const pokeInfo = "pokemon//"
+
+getPokemonList(pokeRange);
+
+function getPokemonList(pokeRange)
+{
+    fetch(mainUrl + limitParam + pokeRange)
+    .then(response => response.json())
+    .then(function(allpokemon) {
+        allpokemon.results.forEach(function(pokemon){
+            fetchPokemonData(pokemon);
+          })
+    });
+}
+
+function fetchPokemonData(pokemon){
+    var url = pokemon.url // <--- this is saving the pokemon url to a      variable to us in a fetch.(Ex: https://pokeapi.co/api/v2/pokemon/1/)
+    fetch(url)
+    .then(response => response.json())
+    .then(function(pokeData){
+        createPokemonCard(pokeData);
+    });
+}
+
+function createPokemonCard(pokeData)
+{
+    var pokeList = document.querySelector('#pokeList');
+
+    fetch(componentsFolder + pokeCardPage)
+    .then(response => response.text())
+    .then(html => {
+
+        var pokeCard = document.createElement('li');
+        pokeCard.classList.add('pokeCard');
+
+        pokeCard.innerHTML = html;
+        var pokeName = pokeCard.querySelector('.pokeName');
+        var pokeId = pokeCard.querySelector('.pokeId');
+        var pokeImg = pokeCard.querySelector('img');
+
+        pokeName.textContent = pokeData.name;
+        pokeId.textContent = "#" + pokeData.order;
+        pokeImg.src = pokeData.sprites.front_default;
+
+        pokeList.appendChild(pokeCard);
+    })
+}
