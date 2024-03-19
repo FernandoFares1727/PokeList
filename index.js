98ef6fee75821dc29d7d3d15cec524430250fa99
FernandoFares1727 <fernandimfares@gmail.com>
2024-03-19 12:02:29 -0300

    Search function method

--- index.js
+++ index.js
@@ -8,13 +8,25 @@ const pokeCardFolder = "PokeCard/"
 const pokeCardPage = "index.html";
 
 const searchs = {
-    tagId : "tagid",
+    tagId : "tagId",
     tagName : "tagName"
 };
 
-//const pokeInfo = "pokemon//"
+loadPage();
 
-getPokemonList(pokeRange);
+function loadPage()
+{
+    enableInput(false);
+    getPokemonList(pokeRange);
+    searchPokemon();
+    enableInput(true);
+}
+
+function enableInput(enable)
+{
+    var searchInput = document.querySelector('#search').querySelector('input');
+    searchInput.disabled = !enable;
+}
 
 function getPokemonList(pokeRange)
 {
@@ -25,6 +37,44 @@ function getPokemonList(pokeRange)
             fetchPokemonData(pokemon);
           })
     });
+}
+
+function searchPokemon()
+{
+    var searchInput = document.querySelector('#search').querySelector('input');
+
+    searchInput.addEventListener("input", function() {
+        var valueInput = searchInput.value.trim().toLowerCase();
+        var actualSearch = document.querySelector('#search').querySelector('button').querySelector('img').getAttribute('id');
+
+        var pokeCards = document.querySelectorAll('.pokeCard');
+
+        if (valueInput == '')
+        {
+            pokeCards.forEach(pokeCard => {
+                pokeCard.style.display = "block";
+            })
+        }
+
+        else if (actualSearch == searchs.tagId)
+        {
+            pokeCards.forEach(pokeCard => {
+                const pokeCardId = pokeCard.querySelector('.pokeId').textContent.replace('#','');
+                const isVisible = pokeCardId == valueInput;
+                pokeCard.style.display = isVisible ? "block" : "none";
+            })
+        }
+
+        else if (actualSearch == searchs.tagName)
+        {
+            valueInput = valueInput
+            pokeCards.forEach(pokeCard => {
+                const pokeName = pokeCard.querySelector('.pokeName').textContent.toLowerCase();
+                const isVisible = pokeName.includes(valueInput);
+                pokeCard.style.display = isVisible ? "block" : "none";
+            })
+        }
+    })
 }
 
 function fetchPokemonData(pokemon){
