16ffbe4f51e2d3eeb76e065916f29d8a0069c8a5
FernandoFares1727 <fernandimfares@gmail.com>
2024-03-23 17:31:23 -0300

    Create poke details card

--- index.js
+++ index.js
@@ -6,10 +6,21 @@ const limitParam = "pokemon?limit=";
 const componentsFolder = "./Components/";
 const pokeCardFolder = "PokeCard/"
 const pokeCardPage = "index.html";
+const pokeDetailsFolder = "PokeDetails/";
+const pokeDetailsCardPage = "index.html";
 
 const searchs = {
     tagId : "tagId",
     tagName : "tagName"
+};
+
+const pokeAtributesLabels = {
+    hp : "HP",
+    attack : "ATK",
+    defense : "DEF",
+    specialAttack : "SATK",
+    speacialDefense : "SDEF",
+    speed : "SPD"
 };
 
 loadPage();
@@ -108,6 +119,13 @@ function createPokemonCard(pokeData)
 
 
         pokeCard.style.order = pokeData.id;
+        pokeCard.setAttribute('id', pokeData.id);
+
+        pokeCard.addEventListener('click', function() {
+            var pokemonId = pokeCard.getAttribute('id');
+            showPokeDetails(pokemonId);
+        })
+
         pokeList.appendChild(pokeCard);
     })
 }
@@ -127,4 +145,157 @@ function changeSearch(element)
         img.src = "./images/tag.svg";
         img.setAttribute('id', searchs.tagId);
     }
+}
+
+function showPokeDetails(pokeId)
+{
+    fetch(mainUrl + "pokemon" + "/" + pokeId + "/")
+    .then(response => response.json())
+    .then(function(pokeData) {
+        createPokemonDetails(pokeData);
+    })
+}
+
+function createPokemonDetails(pokeData)
+{
+    fetch(componentsFolder + pokeCardFolder + pokeDetailsFolder + pokeDetailsCardPage)
+    .then(response => response.text())
+    .then(async html => {
+
+        var pokeDetails = document.createElement('div');
+        pokeDetails.classList.add('pokeDetails');
+
+        pokeDetails.innerHTML = html;
+        var turnBackButton = pokeDetails.querySelector('button');
+
+        turnBackButton.addEventListener('click', function() {
+            removePokeDetails();
+        })
+
+        var pokeDetailsName = pokeDetails.querySelector('.pokeDetailsName');
+        pokeDetailsName.textContent = pokeData.name;
+
+        var pokeDetailsId = pokeDetails.querySelector('.pokeDetailsId');
+        pokeDetailsId.textContent = "#" + pokeData.id;
+
+        var pokeDetailsImage = pokeDetails.querySelector('.pokeDetailsImage').querySelector('img');
+        pokeDetailsImage.src = pokeData.sprites.front_default;
+
+        /* construct pokeDetailsCard */
+
+        var pokeDetailsCard = pokeDetails.querySelector('.pokeDetailsCardInfo');
+        
+        /* construct pokeDetailsTypes */
+
+        var pokeDetailsCardTypes = pokeDetailsCard.querySelector('.pokeDetailsCardTypes');
+        var types = pokeData.types;
+
+        types.forEach(type => {
+            var pokeDetailsCardTypeElement = document.createElement('p');
+            pokeDetailsCardTypeElement.textContent = type.type.name;
+            pokeDetailsCardTypes.appendChild(pokeDetailsCardTypeElement);
+        })
+
+        /* construct pokeDetailsAtributes */
+
+        var pokeDetailsCardWeight = pokeDetailsCard.querySelector('.pokeDetailsCardWeightValue');
+        pokeDetailsCardWeight.textContent = pokeData.weight/10 + "kg";
+
+        var pokeDetailsHeight = pokeDetailsCard.querySelector('.pokeDetailsCardHeightValue');
+        pokeDetailsHeight.textContent = pokeData.height/10 + "m";
+
+        var pokeDetailsMoves = pokeDetails.querySelector('.pokeDetailsCardMovesValue');
+        var moves = pokeData.moves;
+
+        let count = 0;
+        moves.forEach(move => {
+            if (count < 3) {
+                var pokeDetailsCardMoveElement = document.createElement('p');
+                pokeDetailsCardMoveElement.textContent = move.move.name;
+                pokeDetailsMoves.appendChild(pokeDetailsCardMoveElement);
+                count++;
+            } else {
+                return; // Exit the loop early
+            }
+        });
+
+        /* construct description */
+
+        var pokemonDescription = await getPokemonDescription(pokeData.id);
+        var pokeDetailsCardDescription = pokeDetailsCard.querySelector('.pokeDetailsCardDescription');
+        pokeDetailsCardDescription.textContent = pokemonDescription;
+
+        /* construct base stats */
+
+        var pokeDetailsCardStatsLabels = pokeDetailsCard.querySelector('.pokeDetailsCardStatsLabels');
+        var pokeDetailsCardStatsValues = pokeDetailsCard.querySelector('.pokeDetailsCardStatsValues');
+
+        var stats = pokeData.stats;
+        const maxStat = 233;
+
+        stats.forEach(stat => {
+
+            var statIndex = stats.indexOf(stat);
+
+            var statLabel = document.createElement('p');
+            var statName = getShortPokeAtribute(statIndex);
+
+            statLabel.textContent = statName;
+            pokeDetailsCardStatsLabels.appendChild(statLabel);
+
+            var statGrafic = document.createElement('div');
+            statGrafic.classList.add('pokeDetailsCardStatsRows');
+
+            var statValue = document.createElement('p');
+            statValue.textContent = stat.base_stat;
+
+            var statPercentValue = (stat.base_stat / 233) * 100;
+
+            var statGraficBar = document.createElement('div');
+            statGraficBar.style.width = statPercentValue + "%";
+
+            statGrafic.appendChild(statValue);
+            statGrafic.appendChild(statGraficBar);
+
+            pokeDetailsCardStatsValues.appendChild(statGrafic);
+
+        })
+
+        document.body.appendChild(pokeDetails);
+    })
+}
+
+function getShortPokeAtribute(index)
+{
+    switch(index)
+    {
+        case 0:
+            return pokeAtributesLabels.hp;
+        case 1:
+            return pokeAtributesLabels.attack;
+        case 2: 
+            return pokeAtributesLabels.defense;
+        case 3:
+            return pokeAtributesLabels.specialAttack;
+        case 4:
+            return pokeAtributesLabels.speacialDefense;
+        case 5: 
+            return pokeAtributesLabels.speed;
+        default:
+            return pokeAtributesLabels.hp;
+    }
+}
+
+function removePokeDetails()
+{
+    var pokeDetails = document.querySelector('.pokeDetails');
+    document.body.removeChild(pokeDetails);
+}
+
+async function getPokemonDescription(pokeId) {
+  return fetch(mainUrl + "pokemon-species/" + pokeId + "/")
+    .then(response => response.json())
+    .then(function(result) { 
+        return result.flavor_text_entries[0].flavor_text
+    });
 }
